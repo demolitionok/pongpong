caeca722539e601b09ae3335a9d4eea53642a921
demolitionok <demolitionok@gmail.com>
2020-02-09 15:36:32 +0200

    Add SFML

--- pongpong/Program.cs
+++ pongpong/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace pongpong
+﻿using System;
+using SFML.Window;
+using SFML.Graphics;
+using SFML.System;
+
+namespace pongpong
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
+        public static void Main(string[] args)
+        {
+            var window = new RenderWindow(new VideoMode(800, 600), "Test");
+            var shape = new RectangleShape(new Vector2f(50f, 50f))
+            {
+                FillColor = Color.Red
+            };
+            window.Draw(shape);
+            window.Display();
+        }
     }
-  }
 }
