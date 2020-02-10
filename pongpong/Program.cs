fd7d12dfdf62f6d62b481c0bdb1443b8b814b1c4
demolitionok <demolitionok@gmail.com>
2020-02-10 18:02:30 +0200

    Add structured base for game

--- pongpong/Program.cs
+++ pongpong/Program.cs
@@ -1,25 +1,52 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using SFML.Window;
 using SFML.Graphics;
 using SFML.System;
 
 namespace pongpong
 {
+
+    public class Player
+    {
+        public Shape PlayerShape;
+        public List<Keyboard.Key> playerKeys;
+    }
+
+    public class Game
+    {
+        private void Window_KeyPressed(object sender, KeyEventArgs e)
+        {
+            var window = (Window)sender;
+            if (e.Code == Keyboard.Key.Escape)
+            {
+                window.Close();
+            }
+        }
+        public void MakeGraphic()
+        {
+            
+        }
+        public void Run()
+        {
+            var window = new RenderWindow(new VideoMode(800, 600), "dingdong");
+            window.KeyPressed += Window_KeyPressed;
+            while (window.IsOpen)
+            {
+                window.DispatchEvents();
+                window.Display();
+                window.Clear();
+            }
+        }
+    }
+
     internal class Program
     {
         public static void Main(string[] args)
         {
-            var window = new RenderWindow(new VideoMode(800, 600), "Test");
-            var shape = new RectangleShape(new Vector2f(50f, 50f))
-            {
-                FillColor = Color.Red
-            };
-            while (window.IsOpen)
-            {
-                window.Draw(shape);
-                window.Display();
-                window.Clear();
-            }
+            var game = new Game();
+            
         }
     }
 }
