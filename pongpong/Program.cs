5bdfdcc8c48d8944bf3e4656c54f98ca71a33aef
demolitionok <demolitionok@gmail.com>
2020-02-10 19:24:07 +0200

    Modify code base

--- pongpong/Program.cs
+++ pongpong/Program.cs
@@ -11,6 +11,7 @@ namespace pongpong
     public class Player
     {
         public float playerSpeed;
+        public Vector2f dirVector = new Vector2f(0f, 0f);
         public float shapeSize;
         public RectangleShape playerShape;
         public List<Keyboard.Key> playerKeys;
@@ -19,10 +20,20 @@ namespace pongpong
         {
             if (e.Code == playerKeys[0])
             {
-                
+                dirVector = new Vector2f(1,0);
+                movePlayer();
+            }
+            if (e.Code == playerKeys[1])
+            {
+                dirVector = new Vector2f(-1,0);
+                movePlayer();
             }
         }
-
+        public void movePlayer()
+        {
+            Vector2f velocity = dirVector*playerSpeed;
+            playerShape.Position += velocity;
+        }
         public Player(float shapeSize,float playerSpeed, List<Keyboard.Key> playerKeys)
         {
             this.playerKeys = playerKeys;
@@ -33,6 +44,10 @@ namespace pongpong
 
     public class Game
     {
+        public static List<Keyboard.Key> Player1_Keys = new List<Keyboard.Key>();
+        public static List<Keyboard.Key> Player2_Keys = new List<Keyboard.Key>();
+        public Player Player1 = new Player(15f, 5f, Player1_Keys);
+        public Player Player2 = new Player(15f, 5f, Player2_Keys);
         private void Window_KeyPressed(object sender, KeyEventArgs e)
         {
             var window = (Window)sender;
@@ -41,6 +56,20 @@ namespace pongpong
                 window.Close();
             }
         }
+
+        public void InitKeys()
+        {
+            Player1_Keys.Add(Keyboard.Key.F);
+            Player1_Keys.Add(Keyboard.Key.D);
+            Player2_Keys.Add(Keyboard.Key.Right);
+            Player2_Keys.Add(Keyboard.Key.Left);
+        }
+
+        public void Logic()
+        {
+            
+        }
+
         public void MakeGraphic()
         {
             
@@ -49,6 +78,9 @@ namespace pongpong
         {
             var window = new RenderWindow(new VideoMode(800, 600), "dingdong");
             window.KeyPressed += Window_KeyPressed;
+            window.KeyPressed += Player1.Player_KeyPressed;
+            window.KeyPressed += Player2.Player_KeyPressed;
+            
             while (window.IsOpen)
             {
                 window.DispatchEvents();
