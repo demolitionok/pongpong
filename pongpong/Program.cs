53a933941aea10810cae8fb8a50958d9204d5855
demolitionok <demolitionok@gmail.com>
2020-02-12 17:04:36 +0200

    Cut many unnecessary code

--- pongpong/Program.cs
+++ pongpong/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.WebSockets;
 using SFML.Window;
 using SFML.Graphics;
 using SFML.System;
@@ -11,7 +12,7 @@ namespace pongpong
     {
         void InitVertexes();
     }
-    public class BaseObject
+    public class BaseObject : IVertexContaining
     {
         public float speed;
         public Vector2f startPos;
@@ -20,56 +21,52 @@ namespace pongpong
         public Vector2f shapeSize;
         public Shape shape;
 
+        
+        public void InitVertexes()
+        {
+            vertexes.Add(shape.Position);
+            vertexes.Add(new Vector2f(shapeSize.X + shape.Position.X, shape.Position.Y));
+            vertexes.Add(shapeSize + shape.Position);
+            vertexes.Add(new Vector2f(shape.Position.X, shapeSize.Y + shape.Position.Y));
+            /*
+             *    [0]--------[1]
+             *    |            |
+             *    | BaseObject |
+             *    |            |
+             *    [3]--------[2]
+             * 
+             */
+        }
+
         public virtual void MoveObject()
         {
             Vector2f velocity = dirVector * speed;
             shape.Position += velocity;
+            InitVertexes();
         }
     }
 
-    public class Obstacle : BaseObject, IVertexContaining
+    public class Obstacle : BaseObject
     {
-        public RectangleShape obstacleShape;
-        public void InitVertexes()
-        {
-            vertexes.Add(obstacleShape.Position);
-            vertexes.Add(new Vector2f(shapeSize.X + obstacleShape.Position.X, obstacleShape.Position.Y));
-            vertexes.Add(shapeSize + obstacleShape.Position);
-            vertexes.Add(new Vector2f(obstacleShape.Position.X, shapeSize.Y + obstacleShape.Position.Y));
-            /*
-             *    [0]-------------[1]
-             *    |                |
-             *    |    obstacle    |
-             *    |                |
-             *    [3]-------------[2]
-             * 
-             */
-        }
         public Obstacle(Vector2f startPos, Vector2f shapeSize)
         {
             this.startPos = startPos;
             this.shapeSize = shapeSize;
             
-            obstacleShape = new RectangleShape(shapeSize);
-            obstacleShape.Position = startPos;
-            shape = obstacleShape;
+            shape = new RectangleShape(shapeSize);
+            shape.Position = startPos;
             InitVertexes();
         }
     }
 
     public class Puck : BaseObject, IVertexContaining
     {
-        public CircleShape puckShape;
-        public override void MoveObject()
+        public new void InitVertexes()
         {
-            base.MoveObject();
-        }
-        public void InitVertexes()
-        {
-            vertexes.Add(new Vector2f(0.5f * shapeSize.X + puckShape.Position.X, puckShape.Position.Y));
-            vertexes.Add(new Vector2f(shapeSize.X + puckShape.Position.X, 0.5f * shapeSize.Y + puckShape.Position.Y));
-            vertexes.Add(new Vector2f(0.5f * shapeSize.X + puckShape.Position.X, shapeSize.Y + puckShape.Position.Y));
-            vertexes.Add(new Vector2f(puckShape.Position.X, 0.5f * shapeSize.Y + puckShape.Position.Y));
+            vertexes.Add(new Vector2f(0.5f * shapeSize.X + shape.Position.X, shape.Position.Y));
+            vertexes.Add(new Vector2f(shapeSize.X + shape.Position.X, 0.5f * shapeSize.Y + shape.Position.Y));
+            vertexes.Add(new Vector2f(0.5f * shapeSize.X + shape.Position.X, shapeSize.Y + shape.Position.Y));
+            vertexes.Add(new Vector2f(shape.Position.X, 0.5f * shapeSize.Y + shape.Position.Y));
             /*
              *    ./----[0]----\
              *    |            |
@@ -80,23 +77,24 @@ namespace pongpong
              *     It should look like a circle.
              */
         }
-        public Puck(Vector2f shapeSize, float speed, Vector2f dirVector, Vector2f startPos)
+        public Puck(Vector2f shapeSize, float speed, Vector2f dirVector, Vector2f startPos, Color color)
         {
             this.dirVector = dirVector;
             this.startPos = startPos;
             this.speed = speed;
             this.shapeSize = shapeSize;
             
-            puckShape = new CircleShape(shapeSize.X);
-            puckShape.Position = startPos;
-            shape = puckShape;
+            shape = new CircleShape(shapeSize.X)
+            {
+                FillColor = color
+            };
+            shape.Position = startPos;
             InitVertexes();
         }
     }
 
-    public class Player : BaseObject, IVertexContaining
+    public class Player : BaseObject
     {
-        public RectangleShape playerShape;
         public List<Keyboard.Key> playerKeys;
 
         public void Player_KeyPressed(object sender, KeyEventArgs e)
@@ -114,32 +112,15 @@ namespace pongpong
             }
         }
 
-        public void InitVertexes()
-        {
-            vertexes.Add(playerShape.Position);
-            vertexes.Add(new Vector2f(shapeSize.X + playerShape.Position.X, playerShape.Position.Y));
-            vertexes.Add(shapeSize + playerShape.Position);
-            vertexes.Add(new Vector2f(playerShape.Position.X, shapeSize.Y + playerShape.Position.Y));
-            /*
-             *    [0]-------[1]
-             *    |           |
-             *    |   Player  |
-             *    |           |
-             *    [3]-------[2]
-             * 
-             */
-        }
-
         public Player(Vector2f shapeSize, float speed, Vector2f startPos, List<Keyboard.Key> playerKeys)
         {
-            playerShape = new RectangleShape(shapeSize);
+            shape = new RectangleShape(shapeSize);
             this.startPos = startPos;
-            playerShape.Position = startPos;
+            shape.Position = startPos;
 
             this.speed = speed;
             this.playerKeys = playerKeys;
             this.shapeSize = shapeSize;
-            shape = playerShape;
             InitVertexes();
         }
     }
@@ -152,7 +133,7 @@ namespace pongpong
         
         public Player Player1 = new Player(new Vector2f(30f, 20f), 5f, new Vector2f(400, 550), Player1_Keys);
         public Player Player2 = new Player(new Vector2f(30f, 20f), 5f, new Vector2f(400, 50), Player2_Keys);
-        public Puck puck = new Puck(new Vector2f(10f,10f),0.5f,new Vector2f(1,0), new Vector2f(400,300));
+        public Puck puck = new Puck(new Vector2f(10f,10f),0.5f,new Vector2f(1,0), new Vector2f(400,300), Color.Red);
         
         public List<IVertexContaining> VertexContainings = new List<IVertexContaining>();
         public List<BaseObject> BaseObjects = new List<BaseObject>();
@@ -184,7 +165,29 @@ namespace pongpong
 
         public bool DetectCollision(Puck puck, List<BaseObject> figures)
         {
-
+            int count = 0;
+            foreach (var figure in figures)
+            {
+                for(int i = 0; i < puck.vertexes.Count; i++)
+                {
+                    if(
+                        (
+                        (puck.vertexes[i].X >= figure.vertexes[0].X || puck.vertexes[i].X >= figure.vertexes[3].X)
+                        &&
+                        (puck.vertexes[i].X <= figure.vertexes[1].X || puck.vertexes[i].X <= figure.vertexes[2].X)
+                        )
+                        &&
+                        (
+                        (puck.vertexes[i].Y >= figure.vertexes[0].Y || puck.vertexes[i].Y >= figure.vertexes[3].Y)
+                         &&
+                        (puck.vertexes[i].Y <= figure.vertexes[1].Y || puck.vertexes[i].Y <= figure.vertexes[2].Y)
+                        )
+                        )
+                    {
+                        window.Close();
+                    }
+                }   
+            }
             return false;
         }
         public void Logic()
@@ -201,6 +204,7 @@ namespace pongpong
             {
                 window.Draw(figure.shape);
             }
+            window.Draw(puck.shape);
         }
 
         public void Run()
